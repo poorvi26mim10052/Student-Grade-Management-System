016a5ee7f38694e5b02c9b1d2629da25e3ba64a8
poorvi26mim10052 <poorvi.26mim10052@vitbhopal.ac.in>
2026-09-25 19:57:02 +0530

    Update project.py

--- project.py
+++ project.py
@@ -30,6 +30,23 @@ def calculate_percentage():
 
     return percentage, subject_marks
 
+def calculate_grade(percentage):
+
+    if percentage >= 90: 
+        return 'A+'
+    elif percentage >= 80:
+        return 'A'
+    elif percentage >= 70: 
+        return 'B'
+    elif percentage >= 60:
+        return 'C'
+    elif percentage >= 50: 
+        return 'D'
+    else: 
+        return 'F'
+
+
+
 
 
 
